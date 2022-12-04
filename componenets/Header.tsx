95cef70b618be39948d047ca8288f61b3e782eb1
norbusonam <nsonam1822@gmail.com>
2022-12-04 14:20:46 -0500

    header

---
+++ componenets/Header.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Header() {
+  return (
+    <View>
+      <Text style={styles.header}>Todo</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+});
