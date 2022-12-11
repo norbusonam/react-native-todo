7a6dd58694b67cb26a002a2e4dac5a85770c24be
norbusonam <nsonam1822@gmail.com>
2022-12-11 12:18:35 -0500

    turned todo list into state, initial data helper

--- App.tsx
+++ App.tsx
@@ -4,39 +4,17 @@ import Footer from './componenets/Footer';
 import Header from './componenets/Header';
 import TodoList from './componenets/TodoList';
 import { Todo } from './interfaces/Todo';
-
-
-const TodoItems: Todo[] = [
-  {
-    id: 'a',
-    title: 'Get groceries',
-    isCompleted: false,
-  },
-  {
-    id: 'b',
-    title: 'Finish homework',
-    isCompleted: true,
-  },
-  {
-    id: 'c',
-    title: 'Apply to jobs',
-    isCompleted: false,
-  },
-  {
-    id: 'd',
-    title: 'Study for exams',
-    isCompleted: false,
-  },
-]
+import { LongTodoList, ShortTodoList } from './data/InitialTodoLists';
 
 const App: React.FC = () => {
 
+  const [todos, setTodos] = useState(LongTodoList);
   const [isViewingCompleted, setIsViewingCompleted] = useState(false);
 
   const getTodosToDisplay = (): Todo[] => {
     return isViewingCompleted ?
-      TodoItems.filter((todo) => todo.isCompleted) :
-      TodoItems.filter((todo) => !todo.isCompleted)
+      todos.filter((todo) => todo.isCompleted) :
+      todos.filter((todo) => !todo.isCompleted)
   }
 
   const toggleIsViewingCompleted = () => {
